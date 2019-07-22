b8ce6d1c4d1cccc66d0ad35b8d88744ac8491277
gaon000 <gaon2304@gmail.com>
2019-07-22 17:09:46 +0900

    pcap

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ pcap_hello: main.o
 		g++ -g -o pcap_hello main.o -lpcap
 
 main.o:
-		g++ -g -c -o main.o main.cpp
+		g++ -g -c -o main.o ethern.cpp
 
 clean:
 		rm -f pcap_hello
--- ethern.cpp
+++ ethern.cpp
@@ -1,5 +1,14 @@
 #include<pcap.h>
 #include<stdio.h>
+#include<stdint.h>
+#define ETH_ALEN        6
+
+struct ether_header
+{
+  uint8_t  ether_dhost[ETH_ALEN];	/* destination eth addr	*/
+  uint8_t  ether_shost[ETH_ALEN];	/* source ether addr	*/
+  uint16_t ether_type;		        /* packet type ID field	*/
+} __attribute__ ((__packed__));
 
 void usage(){
     printf("syntax: pcap_text <interface\n");
@@ -11,6 +20,7 @@ int main(int argc, char* argv[]){
         usage();
         return -1;
     }
+    
 char * dev = argv[1];
 char errbuf[PCAP_ERRBUF_SIZE];
 pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
@@ -23,11 +33,19 @@ while(true){
     struct pcap_pkthdr* header;
     const u_char* packet;
     int res = pcap_next_ex(handle, &header, &packet);
+    const ether_header * eth = (ether_header*)packet;
+
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
     printf("%u bytes captured\n", header -> caplen);
+    printf("%02X:%02X:%02X:%02X:%02X:%02X\n",eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2], eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5]);
+
 
 }
 pcap_close(handle);
 return 0;
 }
+
+
+
+
